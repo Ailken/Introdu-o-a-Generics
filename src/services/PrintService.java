a699a1b5c6aa66fe1555513d96b68363f1887b36
ailken <ailkencf@gmail.com>
2024-10-05 22:16:19 -0300

    generic implements

--- src/services/PrintService.java
+++ src/services/PrintService.java
@@ -3,15 +3,15 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
+public class PrintService<Type> {
 
-    List<Integer> list = new ArrayList<>();
+    private List<Type> list = new ArrayList<>();
 
-    public void addValue(Integer value) {
+    public void addValue(Type value) {
         list.add(value);
     }
 
-    public Integer first () {
+    public Type first () {
         if (list.isEmpty()) {
             throw new IllegalStateException("List is empty");
         }
@@ -26,6 +26,6 @@ public class PrintService {
         for (int i = 1; i < list.size(); i++) {
             System.out.print(", " + list.get(i));
         }
-        System.out.print("]");
+        System.out.println("]");
     }
 }
